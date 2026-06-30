d906524372abd8e17bbb8f04848c93ef6a047271
Mr. Kartik Saini <104669343+Iamkartiksaini@users.noreply.github.com>
2026-06-30 11:14:21 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -33,8 +33,8 @@ export default function Navbar() {
     setVisible(!isVisible);
   };
 
-  return (<div className="relative">
-    <nav id="navbar" style={{ height: "var(--navbar-height,56px)"}}  className={cn("bg-white sticky flex flex-col top-0 z-50 transition-all duration-300 ease-in-out shadow-sm w-full",
+  return (<div style={{ height: "var(--navbar-height,56px)"}} className="relative">
+    <nav id="navbar" className={cn("bg-white sticky flex flex-col top-0 z-50 transition-all duration-300 ease-in-out shadow-sm w-full",
       isVisible ? "translate-y-0" : "-translate-y-full"
     )}>
       <div className="w-full h-14 max-w-7xl mx-auto px-4 flex items-center justify-between relative z-50 bg-white">
